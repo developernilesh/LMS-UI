0e2487bcab030da4d89abe1dd5aea83634bcb946
developernilesh <nileshmukherjee2017@gmail.com>
2025-02-13 13:47:28 +0530

    Added Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Signup from './pages/Signup.jsx'
-import Login from './pages/Login.jsx'
-import Home from './pages/Home.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from 'react-router-dom'
+import router from './router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
---
+++ src/router.jsx
@@ -0,0 +1,28 @@
+import App from "./App.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
