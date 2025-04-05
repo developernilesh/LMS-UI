1a77873445e0e64f9e7dd1280053358d7edad3d2
developernilesh <nileshmukherjee2017@gmail.com>
2025-04-05 21:22:37 +0530

    Refactored AddCourse component to utilize a new CourseUploadTips component, improving code organization and maintainability.

--- src/components/core/DashBoard/addCourses/AddCourse.jsx
+++ src/components/core/DashBoard/addCourses/AddCourse.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 import CourseInformation from "./CourseInformation";
-import { AiFillThunderbolt } from "react-icons/ai";
-
-const courseUploadTipsTextsArray = [
-  "Set the Course Price option or make it free.",
-  "Standard size for the course thumbnail is 1024x576.",
-  "Video section controls the course overview video.",
-  "Course Builder is where you create & organize a course.",
-  "Add Topics in the Course Builder section to create lessons, quizzes, and assignments.",
-  "Information from the Additional Data section shows up on the course single page.",
-  "Make Announcements to notify any important",
-  "Notes to all enrolled students at once.",
-];
+import CourseUploadTips from "./CourseUploadTips";
 
 const AddCourse = () => {
   return (
@@ -19,19 +8,7 @@ const AddCourse = () => {
       <div className="w-full max-w-[665px]">
         <CourseInformation />
       </div>
-      <div className="w-full max-w-[384px] bg-richblack-800 p-6 flex flex-col gap-5 rounded-md">
-        <div className="flex gap-1 items-center">
-          <AiFillThunderbolt className="text-yellow-100" />
-          <h3 className="text-lg font-semibold text-richblack-5">
-            Course Upload Tips
-          </h3>
-        </div>
-        <div className="flex flex-col gap-2">
-          {courseUploadTipsTextsArray.map((item, index) => (
-            <div className="text-xs" key={index}>&#x2022; {item}</div>
-          ))}
-        </div>
-      </div>
+      <CourseUploadTips/>
     </div>
   );
 };
