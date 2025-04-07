455e115b587f4667609287dd4aa343bce9066461
developernilesh <nileshmukherjee2017@gmail.com>
2025-04-08 01:09:12 +0530

    Refactored ProtectedRoute component to use user state from profile instead of token from auth, improving access control logic.

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-  const { token } = useSelector((state) => state.auth);
+const ProtectedRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.profile);
 
-  if (token) {
+  if (user) {
     return children;
   } else {
     return <Navigate to="/login" />;
