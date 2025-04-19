e47ba81a08453b5716e019aca6c5a97359f8f2a2
developernilesh <nileshmukherjee2017@gmail.com>
2025-04-19 23:56:32 +0530

    Refactored CourseCard component to remove unused key prop, improved status display with conditional styling, and enhanced tag rendering for better visual representation of course details.

--- src/components/core/DashBoard/instructorCourse/CourseCard.jsx
+++ src/components/core/DashBoard/instructorCourse/CourseCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const CourseCard = ({ course, key }) => {
+const CourseCard = ({ course }) => {
   return (
     <div
-      key={key}
       className="w-full max-w-sm border rounded-lg shadow-sm bg-richblack-800 border-richblack-700 hover:scale-[1.01] 
     transtion-all duration-200 ease-in-out cursor-pointer"
     >
@@ -25,22 +24,28 @@ const CourseCard = ({ course, key }) => {
         <p className="text-richblack-200 mb-2 truncate">
           {course.courseDescription}
         </p>
-        <div className="text-richblack-200">
-          Status: <span>{course.status}</span>
+        <div className="text-richblack-200 mb-2">
+          Status:{" "}
+          <span
+            className={`text-sm ${
+              course.status === "Published"
+                ? "bg-caribbeangreen-500"
+                : "bg-pink-600"
+            } rounded-full px-2 text-white`}
+          >
+            {course.status}
+          </span>
         </div>
-        <div className="text-richblack-200  flex justify-between mb-2">
-          <p>
-            Students Enrolled:&nbsp;
-            <span className="text-richblack-50">
-              {course.studentsEnrolled?.length || 0}
-            </span>
-          </p>
-          <p>
-            No. of Reviews:&nbsp;
-            <span className="text-richblack-50">
-              {course.ratingAndReview?.length || 0}
-            </span>
-          </p>
+        <p className="text-richblack-200 mb-2">
+          Students Enrolled:&nbsp;
+          <span className="text-richblack-50">
+            {course.studentsEnrolled?.length || 0}
+          </span>
+        </p>
+        <div className="flex flex-wrap gap-2 text-blue-100">
+          {course.tags?.map((tag, index) => (
+            <span key={index}>#{tag}</span>
+          ))}
         </div>
         <div className="flex justify-end items-center text-richblack-300 gap-2">
           <button className="bg-yellow-50 text-richblack-900 px-3 py-1 rounded-md ">
