df789423fdcfd702f58d84db913f8ffd123473f5
developernilesh <nileshmukherjee2017@gmail.com>
2025-04-26 01:07:44 +0530

    Refactored NestedContent component to utilize Redux for course data, added dynamic rendering of course sections with edit and delete icons, and improved user interaction with caret icons for better UI experience.

--- src/components/core/DashBoard/addCourses/courseBuilder/NestedContent.jsx
+++ src/components/core/DashBoard/addCourses/courseBuilder/NestedContent.jsx
@@ -1,33 +1,72 @@
-import React from "react";
-import { FaCaretRight } from "react-icons/fa";
+import React, { useRef } from "react";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { RiMenuFold2Fill } from "react-icons/ri";
+import { useSelector } from "react-redux";
 
-const NestedContent = () => {
+const NestedContent = ({ editSection }) => {
+  const detailsRef = useRef(null);
+  const { course } = useSelector((state) => state.course);
+
+  const handleCaretClick = (e) => {
+    e.preventDefault();
+    if (detailsRef.current) {
+      detailsRef.current.open = !detailsRef.current.open;
+    }
+  };
+
   return (
-    <div className="bg-richblack-700 px-6 pb-2 rounded-lg border border-richblack-500">
-      <details className="group">
-        <summary className="list-none cursor-pointer border-b border-richblack-500 text-richblack-300">
-          <div className="flex justify-between items-center py-3">
-            <div className="flex gap-1 items-center">
-              <RiMenuFold2Fill />
-              <span>Lesson 1</span>
-            </div>
-            <div className="flex gap-2 items-center">
-              <div className="flex gap-1 items-center border-r border-richblack-300 pr-2">
-                <MdEdit />
-                <MdDelete />
+    <div className="bg-richblack-700 px-6 pb-2 rounded-lg border border-richblack-500 text-richblack-200">
+      {course?.courseContent?.map((item) => (
+        <details className="group" key={item._id}>
+          <summary
+            className="list-none cursor-pointer"
+            // onClick={(e) => e.preventDefault()}
+          >
+            <div className="flex justify-between items-center py-3 border-b border-richblack-500">
+              <div className="flex gap-1 items-center">
+                <AiOutlineMenuUnfold />
+                <span>{item.sectionName}</span>
               </div>
-              <FaCaretRight className="group-open:rotate-90 transition-transform" />
+              <div className="flex gap-2 items-center">
+                <div className="flex gap-1 items-center border-r border-richblack-300 pr-2">
+                  <MdEdit
+                    onClick={(e) => {
+                      e.preventDefault();
+                      editSection(item);
+                    }}
+                  />
+                  <MdDelete />
+                </div>
+                <FaCaretDown
+                  className="group-open:rotate-180 transition-transform cursor-pointer duration-300 ease-linear"
+                  // onClick={handleCaretClick}
+                />
+              </div>
             </div>
+          </summary>
+          <div>
+            {[1, 2, 3, 4].map((_, idx) => (
+              <div
+                className="flex justify-between items-center py-3 border-b border-richblack-500 pl-5"
+                key={idx}
+              >
+                <div className="flex gap-1 items-center">
+                  <RiMenuFold2Fill />
+                  <span>Lesson 1</span>
+                </div>
+                <div className="flex gap-1 items-center">
+                  {/* <div className="flex gap-1 items-center border-r border-richblack-300 pr-2"> */}
+                  <MdEdit />
+                  <MdDelete />
+                  {/* </div> */}
+                </div>
+              </div>
+            ))}
           </div>
-        </summary>
-        <p>
-          Epcot is a theme park at Walt Disney World Resort featuring exciting
-          attractions, international pavilions, award-winning fireworks and
-          seasonal special events.
-        </p>
-      </details>
+        </details>
+      ))}
     </div>
   );
 };
