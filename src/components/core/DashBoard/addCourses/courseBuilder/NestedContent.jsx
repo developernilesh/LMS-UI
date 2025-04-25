1a5be7adb16aec0164049c5928133fa07573fc0b
developernilesh <nileshmukherjee2017@gmail.com>
2025-04-25 23:58:26 +0530

    Enhanced NestedContent component in CourseBuilder by adding icons for editing and deleting lessons, improving the user interface and interaction experience.

--- src/components/core/DashBoard/addCourses/courseBuilder/NestedContent.jsx
+++ src/components/core/DashBoard/addCourses/courseBuilder/NestedContent.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { FaCaretRight } from "react-icons/fa";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { RiMenuFold2Fill } from "react-icons/ri";
 
 const NestedContent = () => {
   return (
-    <div>
-      <details>
-        <summary>Epcot Center</summary>
+    <div className="bg-richblack-700 px-6 pb-2 rounded-lg border border-richblack-500">
+      <details className="group">
+        <summary className="list-none cursor-pointer border-b border-richblack-500 text-richblack-300">
+          <div className="flex justify-between items-center py-3">
+            <div className="flex gap-1 items-center">
+              <RiMenuFold2Fill />
+              <span>Lesson 1</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <div className="flex gap-1 items-center border-r border-richblack-300 pr-2">
+                <MdEdit />
+                <MdDelete />
+              </div>
+              <FaCaretRight className="group-open:rotate-90 transition-transform" />
+            </div>
+          </div>
+        </summary>
         <p>
           Epcot is a theme park at Walt Disney World Resort featuring exciting
           attractions, international pavilions, award-winning fireworks and
