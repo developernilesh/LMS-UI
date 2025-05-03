d142cede3b0950da53eb97956e2ae7ebb48ebde5
developernilesh <nileshmukherjee2017@gmail.com>
2025-05-03 23:55:49 +0530

    Added disabled prop to SubmitButton component to control button state, enhancing user experience during form submission.

--- src/components/Form/SubmitButton.jsx
+++ src/components/Form/SubmitButton.jsx
@@ -4,6 +4,7 @@ const SubmitButton = ({
   buttonContent,
   buttonType,
   onClick,
+  disabled = false,
   width = "w-full",
   background = "bg-yellow-50",
   text = "text-richblack-900",
@@ -14,6 +15,7 @@ const SubmitButton = ({
       className={`${width} ${background} ${hoverState} rounded-md font-medium ${text} px-3 py-1`}
       type={buttonType ? buttonType : "submit"}
       onClick={onClick}
+      disabled={disabled}
     >
       {buttonContent}
     </button>
