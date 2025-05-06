87df510d8ee6b2588c4b647a3af404a0c4a67685
developernilesh <nileshmukherjee2017@gmail.com>
2025-05-06 23:58:50 +0530

    Enhanced ProtectedRoute component to include token expiration handling, dispatching setUser action and clearing local storage when the token expires.

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import { setUser } from "../redux/slices/profileSLice";
 
 const ProtectedRoute = ({ children }) => {
   const { tokenExpiresIn } = useSelector((state) => state.auth);
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (tokenExpiresIn && Date.now() > Number(tokenExpiresIn)) {
+      dispatch(setUser(null));
+      localStorage.removeItem("tokenExpiresIn");
+    }
+  }, [location.pathname]);
 
   if (tokenExpiresIn && Date.now() < Number(tokenExpiresIn)) {
     return children;
