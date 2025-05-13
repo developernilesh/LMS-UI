af95ae1f3d7202729121aa62c12940035ca8e997
developernilesh <nileshmukherjee2017@gmail.com>
2025-05-13 23:59:38 +0530

    Added CategoryCourseCard component to display course details, including thumbnail, name, description, average rating, review count, and formatted price, enhancing the catalog page's UI and user experience.

---
+++ src/components/core/catalogPage/CategoryCourseCard.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import StarRatings from "react-star-ratings";
+
+function formatNumberWithCommas(number) {
+  number = Number(number);
+  if (isNaN(number)) return;
+  return number.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
+const CategoryCourseCard = ({ data }) => {
+  const [avgRating, setAvgRating] = useState(0);
+
+  useEffect(() => {
+    data.ratingAndReview?.length >=1 && setAvgRating(
+      data.ratingAndReview?.reduce((acc, curr) => acc + curr.rating, 0) /
+        data.ratingAndReview?.length
+    );
+  }, []);
+
+  return (
+    <div className="flex flex-col w-[360px] rounded-b-lg bg-richblack-800">
+      <img
+        src={data.thumbNail.secure_url}
+        alt={data.courseName}
+        className="w-full h-[203px] rounded-t-lg"
+      />
+      <div className="p-4 flex flex-col gap-2">
+        <div>
+          <p className="text-richblack-5 font-medium">{data.courseName}</p>
+          <p className="text-richblack-200 text-sm truncate">
+            {data.courseDescription}
+          </p>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div className="text-yellow-100">{avgRating}</div>
+          <StarRatings
+            rating={avgRating}
+            starDimension="16px"
+            starSpacing="2px"
+            starRatedColor="#E7C009"
+            starEmptyColor="#424854"
+            // changeRating={changeRating}
+            numberOfStars={5}
+            name="rating"
+          />
+          <div className="text-richblack-300">
+            ({data.ratingAndReview?.length}{" "}
+            {data.ratingAndReview?.length > 1 ? "reviews" : "review"})
+          </div>
+        </div>
+        <div className="text-lg font-medium text-richblack-5">
+          Rs. {formatNumberWithCommas(data.price)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCourseCard;
