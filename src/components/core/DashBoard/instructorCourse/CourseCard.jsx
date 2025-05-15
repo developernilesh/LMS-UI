5a4aba8de8163a6a35dfbf72308b2523a9ae4951
developernilesh <nileshmukherjee2017@gmail.com>
2025-05-15 23:40:23 +0530

    Refactored CourseCard component layout for improved readability and user experience. Adjusted structure to enhance the display of course details, including status, enrollment count, and tags, while ensuring consistent button placement for editing and deleting courses.

--- src/components/core/DashBoard/instructorCourse/CourseCard.jsx
+++ src/components/core/DashBoard/instructorCourse/CourseCard.jsx
@@ -35,7 +35,7 @@ const CourseCard = ({ course, fetchAllCourses }) => {
     <>
       <div
         className="w-full max-w-sm border rounded-lg shadow-sm bg-richblack-800 border-richblack-700 hover:scale-[1.01] 
-        transtion-all duration-200 ease-in-out cursor-pointer"
+        transtion-all duration-200 ease-in-out"
       >
         <div className="relative">
           <img
@@ -48,64 +48,68 @@ const CourseCard = ({ course, fetchAllCourses }) => {
           </div>
         </div>
 
-        <div className="p-4">
-          <h3 className="text-lg text-richblack-5 font-semibold">
-            {course.courseName}
-          </h3>
-          <p className="text-richblack-200 mb-2 truncate">
-            {course.courseDescription}
-          </p>
-          <div className="text-richblack-200 mb-2">
-            Status:{" "}
-            <span
-              className={`text-sm ${
-                course.status === "Published"
-                  ? "bg-caribbeangreen-500"
-                  : "bg-pink-600"
-              } rounded-full px-2 text-white`}
-            >
-              {course.status}
-            </span>
+        <div className="flex flex-col justify-between px-4 pt-4 min-h-[220px]">
+          <div>
+            <h3 className="text-lg text-richblack-5 font-semibold">
+              {course.courseName}
+            </h3>
+            <p className="text-richblack-200 truncate">
+              {course.courseDescription}
+            </p>
           </div>
-          <p className="text-richblack-200 mb-2">
-            Students Enrolled:&nbsp;
-            <span className="text-richblack-50">
-              {course.studentsEnrolled?.length || 0}
-            </span>
-          </p>
-          <div className="flex flex-wrap gap-2 text-blue-100">
-            {course.tags?.map((tag, index) => (
-              <span key={index}>#{tag}</span>
-            ))}
+          <div className="flex flex-col items-start gap-2">
+            <div className="text-richblack-200">
+              Status:{" "}
+              <span
+                className={`text-sm ${
+                  course.status === "Published"
+                    ? "bg-caribbeangreen-500"
+                    : "bg-pink-600"
+                } rounded-full px-2 text-white`}
+              >
+                {course.status}
+              </span>
+            </div>
+            <p className="text-richblack-200">
+              Students Enrolled:&nbsp;
+              <span className="text-richblack-50">
+                {course.studentsEnrolled?.length || 0}
+              </span>
+            </p>
+            <div className="flex flex-wrap gap-x-2 text-blue-100">
+              {course.tags?.map((tag, index) => (
+                <span key={index}>#{tag}</span>
+              ))}
+            </div>
           </div>
-          <div className="flex justify-end items-center text-richblack-300 gap-2">
-            <SubmitButton
-              buttonContent="Edit"
-              onClick={() =>
-                navigate(`/dashboard/edit-instructor-course/${course._id}`)
-              }
-              buttonType="button"
-              width="w-fit"
-            />
-            <SubmitButton
-              buttonContent="Delete"
-              onClick={() =>
-                setConfirmationModalData({
-                  text1: "Are you sure?",
-                  text2: "Course can have enrolled students!",
-                  btn1text: loading ? "Deleting..." : "Delete",
-                  btn2text: "Cancel",
-                  buttonDisbaled: loading ? true : false,
-                  btn1handler: () => deleteCourse(course._id),
-                  btn2handler: () => setConfirmationModalData(null),
-                })
-              }
-              buttonType="button"
-              width="w-fit"
-              background="bg-pink-200"
-              text="text-richblack-800"
-            />
-          </div>
+        </div>
+        <div className="flex justify-end items-center text-richblack-300 gap-2 px-4 py-3">
+          <SubmitButton
+            buttonContent="Edit"
+            onClick={() =>
+              navigate(`/dashboard/edit-instructor-course/${course._id}`)
+            }
+            buttonType="button"
+            width="w-fit"
+          />
+          <SubmitButton
+            buttonContent="Delete"
+            onClick={() =>
+              setConfirmationModalData({
+                text1: "Are you sure?",
+                text2: "Course can have enrolled students!",
+                btn1text: loading ? "Deleting..." : "Delete",
+                btn2text: "Cancel",
+                buttonDisbaled: loading ? true : false,
+                btn1handler: () => deleteCourse(course._id),
+                btn2handler: () => setConfirmationModalData(null),
+              })
+            }
+            buttonType="button"
+            width="w-fit"
+            background="bg-pink-200"
+            text="text-richblack-800"
+          />
         </div>
       </div>
       {confirmationModalData && (
