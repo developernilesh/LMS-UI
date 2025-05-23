d2bedb4986a29051aabb8a3b8a7e8c17023d3936
developernilesh <nileshmukherjee2017@gmail.com>
2025-05-23 23:58:32 +0530

    Updated CategoryCourseCard to include a Link component for navigation to individual course details, enhancing user experience and interactivity. Adjusted layout for improved responsiveness and visual presentation.

--- src/components/core/catalogPage/CategoryCourseCard.jsx
+++ src/components/core/catalogPage/CategoryCourseCard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import StarRatings from "react-star-ratings";
 
 function formatNumberWithCommas(number) {
@@ -22,43 +23,45 @@ const CategoryCourseCard = ({ data }) => {
   }, []);
 
   return (
-    <div className="flex flex-col w-[360px] rounded-lg my-2 bg-richblack-800 hover:cursor-pointer hover:scale-[1.01] transtion-all duration-200 ease-in-out">
-      <img
-        src={data.thumbNail.secure_url}
-        alt={data.courseName}
-        className="w-full h-[203px] rounded-t-lg"
-      />
-      <div className="p-4 flex flex-col gap-2">
-        <div className="flex flex-col gap-2 min-h-[81px]">
-          <p className="text-richblack-5 font-medium line-clamp-2 overflow-hidden">
-            {data.courseName}
-          </p>
-          <p className="text-richblack-200 truncate italic">
-            {`${data.instructor.firstName} ${data.instructor.lastName}`}
-          </p>
-        </div>
-        <div className="flex gap-2 items-center">
-          <div className="text-yellow-100">{avgRating}</div>
-          <StarRatings
-            rating={avgRating}
-            starDimension="16px"
-            starSpacing="2px"
-            starRatedColor="#E7C009"
-            starEmptyColor="#424854"
-            // changeRating={changeRating}
-            numberOfStars={5}
-            name="rating"
-          />
-          <div className="text-richblack-300">
-            ({data.ratingAndReview?.length}{" "}
-            {data.ratingAndReview?.length > 1 ? "reviews" : "review"})
+    <Link to={`/course/${data._id}`}>
+      <div className="flex flex-col w-full max-w-[360px] rounded-lg my-2 bg-richblack-800 hover:cursor-pointer hover:scale-[1.01] transtion-all duration-200 ease-in-out">
+        <img
+          src={data.thumbNail.secure_url}
+          alt={data.courseName}
+          className="w-full h-auto min-[408px]:h-[203px] rounded-t-lg"
+        />
+        <div className="p-4 flex flex-col gap-2">
+          <div className="flex flex-col gap-2 min-h-[81px]">
+            <p className="text-richblack-5 font-medium line-clamp-2 overflow-hidden">
+              {data.courseName}
+            </p>
+            <p className="text-richblack-200 truncate italic">
+              {`${data.instructor.firstName} ${data.instructor.lastName}`}
+            </p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="text-yellow-100">{avgRating}</div>
+            <StarRatings
+              rating={avgRating}
+              starDimension="16px"
+              starSpacing="2px"
+              starRatedColor="#E7C009"
+              starEmptyColor="#424854"
+              // changeRating={changeRating}
+              numberOfStars={5}
+              name="rating"
+            />
+            <div className="text-richblack-300">
+              ({data.ratingAndReview?.length}{" "}
+              {data.ratingAndReview?.length > 1 ? "reviews" : "review"})
+            </div>
+          </div>
+          <div className="text-lg font-medium text-richblack-5">
+            Rs. {formatNumberWithCommas(data.price)}
           </div>
         </div>
-        <div className="text-lg font-medium text-richblack-5">
-          Rs. {formatNumberWithCommas(data.price)}
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
