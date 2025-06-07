5a6b89ce79badeb50aa5f5fe48d776fe0a7099fe
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-07 23:58:20 +0530

    Added payment-related API endpoints to apiEndpoints.js for capturing and verifying payments, as well as sending payment success emails, enhancing the payment processing capabilities of the application.

--- src/services/apiEndpoints.js
+++ src/services/apiEndpoints.js
@@ -37,7 +37,11 @@ export const endpoints = {
   GET_CART_ITEMS_API: `${BASE_URL}/course/get-cart-items`,
   CLEAR_CART_ITEMS_API: `${BASE_URL}/course/clear-cart`,
   ENROLL_TO_COURSE_API: `${BASE_URL}/course/enroll-to-course`,
-
+  
+  // #region payment endpoints
+  CAPTURE_PAYMENT_API: `${BASE_URL}/payment/capture-payment`,
+  VERIFY_PAYMENT_API: `${BASE_URL}/payment/verify-payment`,
+  SEND_PAYMENT_SUCCESSFUL_EMAIL_API: `${BASE_URL}/payment/payment-successful-email`,
 };
 
 export default endpoints;
