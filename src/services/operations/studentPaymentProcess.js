7175da6b668dee2fddccd4d439df3cecd54b8bf4
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-07 23:58:41 +0530

    Implemented Razorpay payment processing in studentPaymentProcess.js, including payment capture, verification, and success email functionality. Enhanced user experience with toast notifications for payment status and streamlined script loading for the Razorpay SDK.

---
+++ src/services/operations/studentPaymentProcess.js
@@ -0,0 +1,110 @@
+import toast from "react-hot-toast";
+import image from "../../assets/Logo/MainLogo.png";
+import endpoints from "../apiEndpoints";
+import apiConnector from "../apiConnector";
+
+const {
+  CAPTURE_PAYMENT_API,
+  SEND_PAYMENT_SUCCESSFUL_EMAIL_API,
+  VERIFY_PAYMENT_API,
+} = endpoints;
+
+export const payWithRazorpay = async (courses, user, navigate) => {
+  const courseIds = [];
+  courses.forEach((item) => courseIds.push(item._id));
+  if (courseIds.length === 0) {
+    toast.error("No course selected for enrollment!");
+    return;
+  }
+
+  const toastId = toast.loading("Loading...");
+  try {
+    // loading the script
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+    if (!res) {
+      toast.error("Razorpay SDK failed to load");
+      return;
+    }
+    const orderResponse = await apiConnector("POST", CAPTURE_PAYMENT_API, {
+      courseIds,
+    });
+    console.log("order-response", user?.email);
+    if (orderResponse?.data?.success) {
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY,
+        currency: orderResponse?.data?.paymentDetails?.currency,
+        amount: `${orderResponse?.data?.paymentDetails?.amount}`,
+        order_id: orderResponse?.data?.paymentDetails?.id,
+        name: "LearnVerse",
+        description: "Thank You for Purchasing!",
+        image: image,
+        prefill: {
+          name: `${user?.firstName || ""} ${user?.lastName || ""}`,
+          email: user?.email || "",
+          contact: user?.additionalDetails?.contact,
+        },
+        handler: function (response) {
+          sendPaymentSuccessEmail(response, orderResponse);
+          verifyPayment(response, courseIds, navigate);
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+      const paymentObject = new window.Razorpay(options);
+      paymentObject.open();
+      paymentObject.on("payment.failed", function (response) {
+        toast.error("Oops! Payment Failed. Please try again!");
+      });
+    }
+  } catch (error) {
+    toast.error(error?.message || error?.response?.data?.message);
+  } finally {
+    toast.dismiss(toastId);
+  }
+};
+
+const verifyPayment = async (response, courseIds, navigate) => {
+  try {
+    const paymentResponse = await apiConnector("POST", VERIFY_PAYMENT_API, {
+      razorpayOrderid: response.razorpay_order_id,
+      razorpayPaymentId: response.razorpay_payment_id,
+      razorpaySignature: response.razorpay_signature,
+      courses: courseIds,
+    });
+    if (paymentResponse?.data?.success) {
+      toast.success(paymentResponse?.data?.message);
+      navigate("/dashboard/enrolled-courses");
+    }
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+const sendPaymentSuccessEmail = async (response, orderResponse) => {
+  try {
+    await apiConnector("POST", SEND_PAYMENT_SUCCESSFUL_EMAIL_API, {
+      orderId: response.razorpay_order_id,
+      paymentId: response.razorpay_payment_id,
+      amount: orderResponse?.data?.paymentDetails?.amount,
+    });
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+const loadScript = (source) => {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src = source;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+};
