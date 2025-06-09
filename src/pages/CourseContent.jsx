b0f7e0a21196c2ab7d1b2849672a2651b59b2cc9
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-09 23:59:24 +0530

    Enhanced CourseContent component by integrating user validation and loading state management. Added error handling and improved video playback functionality with lecture details display. Updated ContentSidebar to support lecture selection.

--- src/pages/CourseContent.jsx
+++ src/pages/CourseContent.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ContentSidebar from "../components/core/courseContent/ContentSidebar";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setLoading } from "../redux/slices/loaderSlice";
 import apiConnector from "../services/apiConnector";
 import endpoints from "../services/apiEndpoints";
 import toast from "react-hot-toast";
+import Error from "./Error";
+import Loader from "../components/Loader/Loader";
 
 const { GET_SPECIFIC_COURSE_API } = endpoints;
 
@@ -13,7 +15,9 @@ const CourseContent = () => {
   const param = useParams();
   const dispatch = useDispatch();
   const [courseDetails, setCourseDetails] = useState(null);
-  const [videoSource, setVideoSource] = useState(null);
+  const [lecture, setLecture] = useState(null);
+  const { user } = useSelector((state) => state.profile);
+  const { loading } = useSelector((state) => state.loader);
 
   const fetchSpecificCourseDetails = async () => {
     dispatch(setLoading(true));
@@ -24,6 +28,7 @@ const CourseContent = () => {
       );
       if (response?.data?.success) {
         setCourseDetails(response.data.data);
+        setLecture(response.data.data?.courseContent[0]?.subSection[0]);
       }
     } catch (error) {
       toast.error(error?.message || error?.response?.data?.message);
@@ -36,23 +41,29 @@ const CourseContent = () => {
     fetchSpecificCourseDetails();
   }, []);
 
-  return (
+  if (!user || loading) return <Loader />;
+
+  return user?.accountType === "Student" &&
+    user?.courses?.includes(courseDetails?._id) ? (
     <div className="w-full flex flex-col-reverse md:flex-row">
-      <ContentSidebar
-        courseDetails={courseDetails}
-        setVideoSource={setVideoSource}
-      />
-      <div className="w-full py-6">
-        <div className="w-11/12 mx-auto">
+      <ContentSidebar courseDetails={courseDetails} setLecture={setLecture} lecture={lecture} />
+      <div className="w-full">
+        <div className="w-11/12 mx-auto flex flex-col gap-3">
           <video
-            src={videoSource}
+            src={lecture?.SubSectionVideo?.secure_url}
             controls={true}
-            // autoPlay={view ? false : true}
-            className="w-full h-auto"
+            // autoPlay={true}
+            className="w-full"
           />
+          <div className="pt-3 pb-6 w-full border-t border-richblack-600">
+            <h3 className="text-lg font-medium text-richblack-25">{lecture?.title}</h3>
+            <p className="text-sm text-richblack-200">{lecture?.description}</p>
+          </div>
         </div>
       </div>
     </div>
+  ) : (
+    <Error />
   );
 };
 
