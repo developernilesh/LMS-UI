80cd77f610bb1b2d8ea68706574b4b2bc885cdf3
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-10 23:57:31 +0530

    Enhanced ContentSidebar component by integrating user details fetching and lecture marking functionality.

--- src/components/core/courseContent/ContentSidebar.jsx
+++ src/components/core/courseContent/ContentSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SubmitButton from "../../Form/SubmitButton";
 import {
   MdArrowCircleLeft,
@@ -7,73 +7,159 @@ import {
   MdOndemandVideo,
 } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "../../../redux/slices/loaderSlice";
+import apiConnector from "../../../services/apiConnector";
+import endpoints from "../../../services/apiEndpoints";
+import { setUser } from "../../../redux/slices/profileSLice";
+import AddReviewModal from "./AddReviewModal";
+
+const { MARK_UNMARK_LECTURE_API, USER_DETAILS_API } = endpoints;
 
 const ContentSidebar = ({ courseDetails, setLecture, lecture }) => {
-  const [openIndex, setOpenIndex] = useState(0);
+  const [openIndex, setOpenIndex] = useState(null);
+  const [openReviewModal, setopenReviewModal] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.profile);
+
+  const fetchUserDetails = async () => {
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("GET", USER_DETAILS_API);
+      if (response?.data?.success) {
+        dispatch(setUser(response.data.data));
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  const toggleMarkUnmark = async (lectureId) => {
+    dispatch(setLoading(true));
+    try {
+      const response = await apiConnector("POST", MARK_UNMARK_LECTURE_API, {
+        subSectionId: lectureId,
+      });
+      if (response?.data?.success) {
+        fetchUserDetails();
+      }
+    } catch (error) {
+      toast.error(error?.message || error?.response?.data?.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    setOpenIndex(null);
+    setLecture(null);
+    if (!user || courseDetails.courseContent.length === 0) return;
+    if (user.courseProgress?.length > 0) {
+      const lastCompletedLecture =
+        user.courseProgress[user.courseProgress.length - 1];
+      courseDetails.courseContent?.forEach((section, index) => {
+        section.subSection?.forEach((subsection, subindex) => {
+          if (subsection._id === lastCompletedLecture) {
+            setOpenIndex(index);
+            setLecture(subsection);
+          }
+        });
+      });
+    } else {
+      setOpenIndex(0);
+      setLecture(courseDetails.courseContent[0]?.subSection[0]);
+    }
+  }, [user, courseDetails, user.courseProgress.length]);
 
   return (
-    <div className="w-full md:w-[300px] bg-richblack-800 py-6 md:min-h-[calc(100vh-48px)] border-t md:border-t-0 md:border-r border-richblack-700 ">
-      <div className="flex flex-col gap-3 w-11/12 mx-auto border-b border-richblack-500 mb-3 pb-3">
-        <div className="flex gap-3 justify-between items-center">
-          <MdArrowCircleLeft
-            className="scale-[1.75] ml-1 cursor-pointer"
-            onClick={() => navigate("/dashboard/enrolled-courses")}
-          />
-          <SubmitButton
-            buttonContent="Add Review"
-            buttonType="button"
-            width="w-fit"
-          />
-        </div>
-        <h2 className="text-richblack-5 text-lg font-semibold">
-          {courseDetails?.courseName}
-        </h2>
-      </div>
-      <div className="w-full flex flex-col gap-2 cursor-pointer">
-        {courseDetails?.courseContent?.map((item, index) => (
-          <div key={index}>
-            <div
-              className="bg-richblack-700 py-3 text-richblack-25 font-medium"
-              onClick={() => setOpenIndex(openIndex === index ? null : index)}
-            >
-              <div className="w-11/12 mx-auto flex gap-2 justify-between items-center">
-                <p>{item?.sectionName}</p>
-                {openIndex === index ? (
-                  <MdKeyboardArrowUp />
-                ) : (
-                  <MdKeyboardArrowDown />
-                )}
-              </div>
-            </div>
-            {openIndex === index &&
-              item?.subSection?.map((subItem, subIndex) => (
-                <div
-                  className={`border-t border-richblack-600 py-3 text-sm ${
-                    lecture?._id === subItem._id
-                      ? "bg-yellow-700 text-yellow-5"
-                      : "text-richblack-100"
-                  }`}
-                  key={subIndex}
-                  onClick={() => setLecture(subItem)}
-                >
-                  <div className="w-11/12 mx-auto flex gap-2 items-center justify-between">
-                    <div className="flex gap-2 items-center">
-                      <input 
-                        type="checkbox"
-                        className="w-4 h-4 rounded border-richblack-300 bg-richblack-700 text-yellow-25 focus:ring-yellow-25 focus:ring-offset-richblack-700 cursor-pointer"
-                      />
-                      <p>{subItem.title}</p>
-                      <MdOndemandVideo className="text-richblack-300" />
-                    </div>
-                    <div>{convertTime(subItem?.SubSectionVideo?.duration)}</div>
-                  </div>
-                </div>
-              ))}
+    <>
+      <div className="w-full md:w-[300px] bg-richblack-800 py-6 md:min-h-[calc(100vh-48px)] border-t md:border-t-0 md:border-r border-richblack-700 ">
+        <div className="flex flex-col gap-3 w-11/12 mx-auto border-b border-richblack-500 mb-3 pb-3">
+          <div className="flex gap-3 justify-between items-center">
+            <MdArrowCircleLeft
+              className="scale-[1.75] ml-1 cursor-pointer"
+              onClick={() => navigate("/dashboard/enrolled-courses")}
+            />
+            <SubmitButton
+              buttonContent="Add Review"
+              buttonType="button"
+              width="w-fit"
+              onClick={() => setopenReviewModal(true)}
+            />
           </div>
-        ))}
+          <h2 className="text-richblack-5 text-lg font-semibold">
+            {courseDetails?.courseName}
+          </h2>
+          <div>
+            {user.courseProgress.length}/
+            {courseDetails.courseContent?.reduce(
+              (acc, curr) => acc + curr.subSection.length,
+              0
+            )}{" "}
+            Lectures Completed
+          </div>
+        </div>
+        <div className="w-full flex flex-col gap-2 cursor-pointer">
+          {courseDetails?.courseContent?.map((item, index) => (
+            <div key={index}>
+              <div
+                className="bg-richblack-700 py-3 text-richblack-25 font-medium"
+                onClick={() => setOpenIndex(openIndex === index ? null : index)}
+              >
+                <div className="w-11/12 mx-auto flex gap-2 justify-between items-center">
+                  <p>{item?.sectionName}</p>
+                  {openIndex === index ? (
+                    <MdKeyboardArrowUp />
+                  ) : (
+                    <MdKeyboardArrowDown />
+                  )}
+                </div>
+              </div>
+              {openIndex === index &&
+                item?.subSection?.map((subItem, subIndex) => (
+                  <div
+                    className={`border-t border-richblack-600 py-3 text-sm ${
+                      lecture?._id === subItem._id
+                        ? "bg-yellow-700 text-yellow-5"
+                        : "text-richblack-100"
+                    }`}
+                    key={subIndex}
+                    onClick={() => setLecture(subItem)}
+                  >
+                    <div className="w-11/12 mx-auto flex gap-2 items-center justify-between">
+                      <div className="flex gap-2 items-center">
+                        <input
+                          type="checkbox"
+                          onChange={(e) => {
+                            toggleMarkUnmark(subItem._id);
+                          }}
+                          checked={
+                            user?.courseProgress?.includes(subItem._id)
+                              ? true
+                              : false
+                          }
+                          className="w-4 h-4 rounded border-richblack-300 bg-richblack-700 text-yellow-25 focus:ring-yellow-25 focus:ring-offset-richblack-700 cursor-pointer"
+                        />
+                        <p>{subItem.title}</p>
+                        <MdOndemandVideo className="text-richblack-300" />
+                      </div>
+                      <div>
+                        {convertTime(subItem?.SubSectionVideo?.duration)}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      {openReviewModal && (
+        <AddReviewModal setopenReviewModal={setopenReviewModal} />
+      )}
+    </>
   );
 };
 
