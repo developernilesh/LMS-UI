c7d52dda8f7410f5df21beee0cb74b36a4bf8d06
developernilesh <nileshmukherjee2017@gmail.com>
2025-06-13 23:59:46 +0530

    Refactored loading state management in EnrolledCourse component to use local state instead of Redux, improving component independence and simplifying loading logic.

--- src/components/core/DashBoard/enrolledCourses/EnrolledCourse.jsx
+++ src/components/core/DashBoard/enrolledCourses/EnrolledCourse.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from "react";
 import CourseCard from "./CourseCard";
 import endpoints from "../../../../services/apiEndpoints";
-import { setLoading } from "../../../../redux/slices/loaderSlice";
-import { useDispatch, useSelector } from "react-redux";
 import apiConnector from "../../../../services/apiConnector";
 import toast from "react-hot-toast";
-import Loader from "../../../Loader/Loader";
 import CourseCardSkeleton from "./CourseCardSkeleton";
+import { useSelector } from "react-redux";
 
 const { VIEW_ENROLLED_COURSES_API } = endpoints;
 
 const EnrolledCourse = () => {
   const [enrolledCourses, setEnrolledCourses] = useState([]);
-  const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.loader);
+  const [loading, setLoading] = useState([]);
   const { user } = useSelector((state) => state.profile);
 
   const fetchEnrolledCourses = async () => {
-    dispatch(setLoading(true));
+    setLoading(true);
     try {
       const response = await apiConnector("GET", VIEW_ENROLLED_COURSES_API);
       if (response?.data?.success) {
@@ -26,7 +23,7 @@ const EnrolledCourse = () => {
     } catch (error) {
       toast.error(error?.response?.data?.message || "Something Went Wrong!");
     } finally {
-      dispatch(setLoading(false));
+      setLoading(false);
     }
   };
 
@@ -35,13 +32,18 @@ const EnrolledCourse = () => {
   }, []);
 
   if (!user?._id || loading) {
-    <div className="w-11/12">
-      <div className="flex flex-wrap justify-center gap-6">
-        {[1, 2].map((_, index) => (
-          <CourseCardSkeleton key={index} />
-        ))}
+    return (
+      <div className="w-11/12">
+        <h3 className="text-3xl text-richblack-5 font-medium py-3">
+          Enrolled Courses
+        </h3>
+        <div className="flex flex-wrap justify-center gap-6">
+          {[1, 2].map((_, index) => (
+            <CourseCardSkeleton key={index} />
+          ))}
+        </div>
       </div>
-    </div>;
+    );
   }
 
   return (
