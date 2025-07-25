3c26bd17df0fdfed72858043bde84ec0928cf795
developernilesh <nileshmukherjee2017@gmail.com>
2025-07-26 00:48:18 +0530

    Removed some components from HomePage to streamline the codebase and eliminate unused elements.

--- src/components/core/HomePage/HighlightedText.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HighlightedText = ({children}) => {
-  return <span className="bg-gradient-to-br from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text">{children}</span>;
-};
-
-export default HighlightedText;
